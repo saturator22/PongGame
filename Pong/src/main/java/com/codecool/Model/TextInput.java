8ed64583e5d9c5420a2d61f944a159db9a5ed7c5
WojTechM <wojciech.m1997@gmail.com>
2018-07-16 18:39:59 +0200

    Create sample model for user input

---
+++ Pong/src/main/java/com/codecool/Model/TextInput.java
@@ -0,0 +1,14 @@
+package com.codecool.Model;
+
+public class TextInput {
+
+    private String input;
+
+    public TextInput(String input) {
+        this.input = input;
+    }
+
+    public String toString() {
+        return input;
+    }
+}
