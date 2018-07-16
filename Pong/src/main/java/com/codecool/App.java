f0a572e77bc230c4d89e10c0fd82c1d49518a63b
saturator22 <f.szczeski@gmail.com>
2018-07-16 14:32:38 +0200

    Set up server in Java

--- Pong/src/main/java/com/codecool/App.java
+++ Pong/src/main/java/com/codecool/App.java
@@ -1,13 +1,19 @@
 package com.codecool;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // create a server on port 8000
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        // set routes
+//        server.createContext("/", new MainMenu());
+        server.createContext("/test", new TestHandler());
+//        server.createContext("/mentorShop", new MentorShopHandler());
+        server.setExecutor(null); // creates a default executor
+        // start listening
+        server.start();
     }
 }
