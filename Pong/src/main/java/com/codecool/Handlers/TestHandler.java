ea809c4f57a7a976d18cdb7222d73580a9e98833
WojTechM <wojciech.m1997@gmail.com>
2018-07-16 14:51:21 +0200

    Update TestHandler

--- Pong/src/main/java/com/codecool/Handlers/TestHandler.java
+++ Pong/src/main/java/com/codecool/Handlers/TestHandler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class TestHandler implements HttpHandler {
     final String GET_METHOD = "GET";
     final String POST_METHOD = "POST";
+    private String textResponse = "loading";
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
@@ -15,7 +16,21 @@ public class TestHandler implements HttpHandler {
         String method = httpExchange.getRequestMethod();
 
         if (method.equals(GET_METHOD) {
-            
+            sendResponse(httpExchange, textResponse);
+        } else if (method.equals(POST_METHOD)) {
+            sendResponse(httpExchange, textResponse);
+        }
+    }
+
+    public void sendResponse(HttpExchange exchange, String response) {
+        byte[] bytes = response.getBytes();
+        try {
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
