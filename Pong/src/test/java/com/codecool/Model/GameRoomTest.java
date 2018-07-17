25d978e18e4010f8d537768875588bed9f6f9539
WojTechM <wojciech.m1997@gmail.com>
2018-07-17 15:34:12 +0200

    Update GameRoom class test

--- Pong/src/test/java/com/codecool/Model/GameRoomTest.java
+++ Pong/src/test/java/com/codecool/Model/GameRoomTest.java
@@ -9,11 +9,16 @@ public class GameRoomTest {
     @Test
     public void Should_ReturnValidJSON_When_CorrectInput() {
         Player player = new Player(2.3f, "nickname");
-        Ball ball = new Ball(0f, 0f, 90f, 10f);
-        GameRoom gameRoom = new GameRoom(ball, 60f, player, player);
-        String expected = "{\"ball\":{\"xPos\":0.0,\"yPos\":0.0,\"angle\":90.0,\"speed\":10.0}," +
-                "\"racketHeight\":60.0,\"firstPlayer\":{\"racketYPos\":2.3,\"name\":\"nickname\",\"score\":0}," +
-                "\"secondPlayer\":{\"racketYPos\":2.3,\"name\":\"nickname\",\"score\":0}}";
+        Ball ball = new Ball(0f, 0f, 90f, 10f, 0f);
+        GameRoom gameRoom = new GameRoom(ball, player, player);
+        String expected = "{\"ball\":{\"xPos\":0.0,\"yPos\":0.0,\"angle\":90.0,\"speed\":10.0," +
+                    "\"speedIncreaseRate\":0.0,\"ballSize\":10.0}," +
+
+                "\"firstPlayer\":{\"racketYPos\":2.3,\"racketXPos\":5.0,\"name\":\"nickname\",\"score\":0," +
+                    "\"racketHeight\":60.0,\"racketWidth\":10.0}," +
+
+                "\"secondPlayer\":{\"racketYPos\":2.3,\"racketXPos\":5.0,\"name\":\"nickname\",\"score\":0," +
+                    "\"racketHeight\":60.0,\"racketWidth\":10.0}}";
         assertEquals(expected, gameRoom.toJSON());
     }
 }
