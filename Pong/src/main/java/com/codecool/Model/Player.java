0477771551b2c0254c884ba8567948facd8f13b6
WojTechM <wojciech.m1997@gmail.com>
2018-07-17 15:33:28 +0200

    Refactor Player class

--- Pong/src/main/java/com/codecool/Model/Player.java
+++ Pong/src/main/java/com/codecool/Model/Player.java
@@ -2,13 +2,28 @@ package com.codecool.Model;
 
 public class Player {
     private float racketYPos;
+    private final float racketXPos;
     private String name;
     private int score;
+    private final float racketHeight;
+    private final float racketWidth;
 
     public Player(float racketYPos, String name) {
         this.racketYPos = racketYPos;
+        this.racketXPos = 5; // Default value
         this.name = name;
-        this.score = 0;
+        this.score = 0; // Default value
+        this.racketHeight = 60; // Default value
+        this.racketWidth = 10; // Default value
+    }
+
+    public Player(float racketYPos, float racketXPos, String name, int score, float racketHeight, float racketWidth) {
+        this.racketYPos = racketYPos;
+        this.racketXPos = racketXPos;
+        this.name = name;
+        this.score = score;
+        this.racketHeight = racketHeight;
+        this.racketWidth = racketWidth;
     }
 
     public float getRacketYPos() {
@@ -17,6 +32,10 @@ public class Player {
 
     public void setRacketYPos(float racketYPos) {
         this.racketYPos = racketYPos;
+    }
+
+    public float getRacketXPos() {
+        return racketXPos;
     }
 
     public String getName() {
@@ -33,5 +52,13 @@ public class Player {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public float getRacketHeight() {
+        return racketHeight;
+    }
+
+    public float getRacketWidth() {
+        return racketWidth;
     }
 }
