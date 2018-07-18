0d12b1d6bfff83192ab0494ab698988e23a648af
WojTechM <wojciech.m1997@gmail.com>
2018-07-18 09:54:33 +0200

    Add changePosition method to Player class

--- Pong/src/main/java/com/codecool/Model/Player.java
+++ Pong/src/main/java/com/codecool/Model/Player.java
@@ -61,4 +61,12 @@ public class Player {
     public float getRacketWidth() {
         return racketWidth;
     }
+
+    public void changePosition(String direction) {
+        if (direction.equals("up")) {
+            this.racketYPos -= 10;
+        } else {
+            this.racketYPos += 10;
+        }
+    }
 }
