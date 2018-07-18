24202f4591aca12ad57d5cf902a94da9585bee6e
saturator22 <f.szczeski@gmail.com>
2018-07-18 16:34:09 +0200

    Add dependency injection into updateGameboard, methods for operating on static gameRooms map

--- Pong/src/main/java/com/codecool/Handlers/TestHandler.java
+++ Pong/src/main/java/com/codecool/Handlers/TestHandler.java
@@ -11,34 +11,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpCookie;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestHandler implements HttpHandler {
     final String GET_METHOD = "GET";
     final String POST_METHOD = "POST";
-    private GameRoom gameRoom;
-    private String textResponse;
-
-    public TestHandler(GameRoom gameRoom) {
-        this.gameRoom = gameRoom;
-    }
+    private static Map<String, GameRoom> gameRooms = new HashMap<>();
 
     @Override
     public void handle(HttpExchange httpExchange) {
-
+        String cookieStr = httpExchange.getResponseHeaders().getFirst("Cookie");
+        HttpCookie cookie = HttpCookie.parse(cookieStr).get(0);
         String method = httpExchange.getRequestMethod();
 
+        GameRoom gameRoom = gameRooms.get(cookie.getValue());
+
         if (method.equals(GET_METHOD)) {
-            updateGameroom();
+            updateGameroom(gameRoom);
             sendResponse(httpExchange, gameRoom.toJSON());
+
         } else if (method.equals(POST_METHOD)) {
+            String player = getPlayerFromCookie(cookie);
             TextInput input = readAndParseJSON(httpExchange);
-            gameRoom.getFirstPlayer().changePosition(input.toString());
-            textResponse = "";
+
+            if(player.equalsIgnoreCase("p1")) {
+                gameRoom.getFirstPlayer().changePosition(input.toString());
+
+            } else if(player.equalsIgnoreCase("p2")) {
+                gameRoom.getSecondPlayer().changePosition(input.toString());
+            }
+
+            String textResponse = "";
             sendResponse(httpExchange, textResponse);
         }
     }
 
-    public void updateGameroom() {
+    private String getPlayerFromCookie(HttpCookie cookie) {
+        return cookie.getComment().split(":")[0];
+    }
+
+    public static Map<String, GameRoom> getGameRooms() {
+        return gameRooms;
+    }
+
+    public static void addToGameRooms(String roomId, GameRoom gameRoom) {
+        gameRooms.put(roomId, gameRoom);
+    }
+
+    public static void removeFromGameRooms(String roomId) {
+        gameRooms.remove(roomId);
+    }
+
+    public void updateGameroom(GameRoom gameRoom) {
         Ball ball = gameRoom.getBall();
         ball.updateAngleIfOnBoardEdge();
         ball.updateAngleIfCollision(gameRoom.getFirstPlayer());
