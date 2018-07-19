ceea99a150e351fa2e6fb8d27b2a8f09535aa719
WojTechM <wojciech.m1997@gmail.com>
2018-07-19 20:05:54 +0200

    Implement update of nicnkames and scores on change

--- Pong/src/main/resources/static/js/rackets.js
+++ Pong/src/main/resources/static/js/rackets.js
@@ -19,17 +19,34 @@ class Ball {
 
 class GameBoard {
     constructor() {
-        this.racket1 = new Racket(240, 5, "Wojtech");
-        this.racket2 = new Racket(240, 785, "Bart");
+        this.racket1 = new Racket(240, 5, "Player One");
+        this.racket2 = new Racket(240, 775, "Player Two");
         this.ball = new Ball(400, 240, 10);
         this.renderPlayersNames();
     }
 
     renderPlayersNames() {
-      let player1name = document.getElementById("player1name");
-      player1name.innerHTML = this.racket1.name;
-      let player2name = document.getElementById("player2name");
-      player2name.innerHTML = this.racket2.name;
+        let player1name = document.getElementById("player1name");
+        if (player1name.innerHTML == "Player One") {
+            player1name.innerHTML = this.racket1.name;
+        }
+
+        let player2name = document.getElementById("player2name");
+        if (player2name.innerHTML == "Player Two") {
+            player2name.innerHTML = this.racket2.name;
+        }
+    }
+
+    renderScores() {
+        let player1score = document.getElementById("player1score");
+        if (player1score.innerHTML != this.racket1.score) {
+            player1score.innerHTML = this.racket1.score;
+        }
+
+        let player2score = document.getElementById("player2score");
+        if (player2score.innerHTML != this.racket2.score) {
+            player2score.innerHTML = this.racket2.score;
+        }
     }
 
     resetScores() {
@@ -65,6 +82,9 @@ class GameBoard {
         gameLine.stroke();
         racket1View.stroke();
         racket2View.stroke();
+
+        this.renderPlayersNames();
+        this.renderScores();
     }
 
     clear() {
@@ -103,7 +123,13 @@ class GameBoard {
 
     updateRackets(parsedJson) {
         this.racket1.yPosition = parsedJson.firstPlayer.racketYPos;
+        this.racket1.name = parsedJson.firstPlayer.name;
+        this.racket1.score = parsedJson.firstPlayer.score;
+
         this.racket2.yPosition = parsedJson.secondPlayer.racketYPos;
+        this.racket2.name = parsedJson.secondPlayer.name;
+        this.racket2.score = parsedJson.secondPlayer.score;
+
     }
 
     sendInput(direction) { // POST
