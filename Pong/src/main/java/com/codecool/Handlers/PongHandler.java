ffc2465bfb846d20e8c7880c8bffae2868049e2b
WojTechM <wojciech.m1997@gmail.com>
2018-07-19 09:16:45 +0200

    Refactor

--- Pong/src/main/java/com/codecool/Handlers/PongHandler.java
+++ Pong/src/main/java/com/codecool/Handlers/PongHandler.java
@@ -20,64 +20,67 @@ public class PongHandler implements HttpHandler {
     public void handle(HttpExchange exchange) {
         String method = exchange.getRequestMethod();
         String cookieStr = exchange.getResponseHeaders().getFirst("Cookie");
-        HttpCookie cookie;
-
         try {
-
-            if(method.equals("GET")) {
-                if(cookieStr != null) {
+            if (method.equals("GET")) {
+                if (cookieStr != null) {
                     exchange.getResponseHeaders().add("Set-Cookie", cookieStr + ";Max-Age=0");
                 }
-                String html = loadSite("static/html/index.html");
+                String html = loadSite("/static/html/index.html");
                 sendResponse(exchange, html);
             } else if (method.equals("POST")) {
-                Map<String, String> parsedFormData = Redirector.getPostStringData(exchange);
-                String nickName = parsedFormData.get("nickName");
-                String roomId = parsedFormData.get("roomId");
-                System.out.println(nickName);
-                System.out.println(3);
-
-                cookie = assignCookieToGameRoom(exchange, roomId, nickName);
-                exchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
-
-                String html = loadSite("static/html/index.html");
-                sendResponse(exchange, html);
+                handlePost(exchange);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void handlePost(HttpExchange exchange) throws IOException {
+        HttpCookie cookie;
+        Map<String, String> parsedFormData = Redirector.getPostStringData(exchange);
+        String nickName = parsedFormData.get("nickName");
+        String roomId = parsedFormData.get("roomId");
+
+        cookie = assignCookieToGameRoom(exchange, roomId, nickName);
+        String cookieHeader = convertCookieToString(cookie);
+        exchange.getResponseHeaders().add("Set-Cookie", cookieHeader);
+
+        sendResponse(exchange, "TODO");
+    }
+
+    private String convertCookieToString(HttpCookie cookie) {
+        String result = "roomId=" + cookie.getValue() + "&player=" + cookie.getComment();
+        return result;
+    }
+
     public HttpCookie assignCookieToGameRoom(HttpExchange exchange, String roomId, String nickName) {
-            Map<String, GameRoom> gameRooms = TestHandler.getGameRooms();
-            HttpCookie cookie = null;
-            Ball ball = new Ball(400f, 240f, 15f, 10f, 0.02f);
-            Player player1;
-            Player player2;
-            GameRoom gameRoom = new GameRoom(ball, null, null);
+        Map<String, GameRoom> gameRooms = TestHandler.getGameRooms();
+        HttpCookie cookie = null;
+        Ball ball = new Ball(400f, 240f, 15f, 10f, 0.02f);
+        Player player1;
+        Player player2;
+        GameRoom gameRoom = new GameRoom(ball, null, null);
 
         try {
-            if(!isRoomCreated(roomId)) {
+            if (!isRoomCreated(roomId)) {
                 cookie = createCookie(nickName, roomId, "P1");
                 player1 = new Player(240f, 5f, nickName, 0, 60f, 10f);
                 gameRoom.setFirstPlayer(player1);
                 TestHandler.addToGameRooms(roomId, gameRoom);
-
-            } else if((!isRoomCreated(roomId) && gameRooms.get(roomId).getSecondPlayer() == null)) {
+            } else if ((isRoomCreated(roomId) && gameRooms.get(roomId).getSecondPlayer() == null)) {
                 GameRoom gameRoomToJoin = gameRooms.get(roomId);
                 cookie = createCookie(nickName, roomId, "P2");
                 player2 = new Player(240f, 795f, nickName, 0, 60f, 10f);
                 gameRoomToJoin.setSecondPlayer(player2);
                 TestHandler.addToGameRooms(roomId, gameRoomToJoin);
 
-            } else if((!isRoomCreated(roomId) && gameRooms.get(roomId).getSecondPlayer() != null)) {
+            } else if ((!isRoomCreated(roomId) && gameRooms.get(roomId).getSecondPlayer() != null)) {
                 Redirector.redirect(exchange, "/pong");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-            return cookie;
+        return cookie;
     }
 
     private boolean isRoomCreated(String roomId) {
@@ -90,10 +93,8 @@ public class PongHandler implements HttpHandler {
     public HttpCookie createCookie(String nickName, String roomId, String player) {
         HttpCookie cookie = new HttpCookie("sessionId", roomId);
         cookie.setComment(player + ":" + nickName);
-
         return cookie;
     }
-//    public  isCookieValid()
 
     public String loadSite(String fileName) {
         StringBuilder result = new StringBuilder();
