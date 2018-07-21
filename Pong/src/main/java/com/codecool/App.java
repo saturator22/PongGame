fe70f2e071d6c6af251da79f071261a693c2daab
WojTechM <wojciech.m1997@gmail.com>
2018-07-21 20:19:20 +0200

    Refactor App - send GameController object to http handlers

--- Pong/src/main/java/com/codecool/App.java
+++ Pong/src/main/java/com/codecool/App.java
@@ -1,6 +1,6 @@
 package com.codecool;
 
-import com.codecool.GameControllers.PhysicsController;
+import com.codecool.GameControllers.*;
 import com.codecool.Handlers.PongHandler;
 import com.codecool.Handlers.ResetHandler;
 import com.codecool.Handlers.StaticHandler;
@@ -14,14 +14,17 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         PhysicsController physicsController = new PhysicsController(800, 480);
+        PlayerController playerController = new PlayerController();
+        GameController gameController = new GameController(physicsController, playerController);
+
 
         // create a server on port 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
-        server.createContext("/game", new GameHandler(physicsController));
-        server.createContext("/reset", new ResetHandler());
-        server.createContext("/pong", new PongHandler());
+        server.createContext("/game", new GameHandler(gameController));
+        server.createContext("/reset", new ResetHandler(gameController));
+        server.createContext("/pong", new PongHandler(gameController));
         server.createContext("/static", new StaticHandler());
         // start listening
         server.start();
