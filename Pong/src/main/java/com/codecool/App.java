c4917ca1479297c5aab4f3ff2b65e38ea1dc620c
WojTechM <wojciech.m1997@gmail.com>
2018-07-21 18:37:21 +0200

    Create and send PhysicsController object to TestHandler

--- Pong/src/main/java/com/codecool/App.java
+++ Pong/src/main/java/com/codecool/App.java
@@ -1,5 +1,6 @@
 package com.codecool;
 
+import com.codecool.GameControllers.PhysicsController;
 import com.codecool.Handlers.PongHandler;
 import com.codecool.Handlers.ResetHandler;
 import com.codecool.Handlers.StaticHandler;
@@ -12,11 +13,13 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        PhysicsController physicsController = new PhysicsController(800, 480);
+
         // create a server on port 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
-        server.createContext("/test", new TestHandler());
+        server.createContext("/test", new TestHandler(physicsController));
         server.createContext("/reset", new ResetHandler());
         server.createContext("/pong", new PongHandler());
         server.createContext("/static", new StaticHandler());
