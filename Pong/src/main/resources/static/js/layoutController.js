8e018d20cd3556b4c3c8cafc60c880d2267d9ad2
dthprf <34771904+dthprf@users.noreply.github.com>
2018-08-24 15:55:15 +0200

    Display bug fixes

--- Pong/src/main/resources/static/js/layoutController.js
+++ Pong/src/main/resources/static/js/layoutController.js
@@ -6,6 +6,7 @@ window.onload = function () {
     layoutController.aboutButtonHandler();
     layoutController.submitButtonEventHandler();
     layoutController.resetButtonEventHandler();
+    layoutController.initSidebar();
 
 }
 
@@ -155,5 +156,38 @@ class LayoutController {
 
 		let board = document.getElementById('myCanvas');
 		board.className = "displayInline";
-	}
+    }
+    
+    initSidebar() {
+        var sidebarBox = document.querySelector('#box');
+        var sidebarBtn = document.querySelector('#btn');
+        var pageWrapper = document.querySelector('#boardContainer');
+
+        sidebarBtn.addEventListener('click', function (event) {
+
+            if (this.classList.contains('active')) {
+                this.classList.remove('active');
+                sidebarBox.classList.remove('active');
+            } else {
+                this.classList.add('active');
+                sidebarBox.classList.add('active');
+            }
+        });
+
+        pageWrapper.addEventListener('click', function (event) {
+
+            if (sidebarBox.classList.contains('active')) {
+                sidebarBtn.classList.remove('active');
+                sidebarBox.classList.remove('active');
+            }
+        });
+
+        window.addEventListener('keydown', function (event) {
+
+            if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+                sidebarBtn.classList.remove('active');
+                sidebarBox.classList.remove('active');
+            }
+        });
+    }
 }
